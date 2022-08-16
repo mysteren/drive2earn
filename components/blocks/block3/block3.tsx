b6d35584f837976dd119f0ad66494a88eb0863b7
Timofey <mysterenct@gmail.com>
2022-08-16 22:36:43 +0300

    second border-polygon

--- components/blocks/block3/block3.tsx
+++ components/blocks/block3/block3.tsx
@@ -5,14 +5,14 @@ import Title1 from "../../ui/title1/title1";
 import styles from "./block3.module.css";
 
 interface Props {
-    children: JSX.Element | JSX.Element[];
+  children: JSX.Element | JSX.Element[];
 }
-
 
 const Block3: FunctionComponent<Props> = ({ children }) => {
   return (
     <div className={styles["container"]}>
-        { children }
+      <div className={styles["top-border"]}></div>
+      {children}
     </div>
   );
 };
